41b31978ec9d8c3551c6c67ada4b46d21d916ad8
aFriendlySword <afriendlysword@gmail.com>
2017-09-15 10:55:47 +0200

    e

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ app.set('port', (process.env.PORT || 8080));
 app.get('/',function(req, res) {
         res.sendFile(__dirname + '/index.html');
 });
-
+app.get('/style.css',function(req, res) {
+        res.sendFile(__dirname + '/style.css');
+});
+app.get('/Client.js',function(req, res) {
+        res.sendFile(__dirname + '/Client.js');
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
